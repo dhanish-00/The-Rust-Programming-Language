d8521911ed380bf2b36a175020bb425dcff90668
dhanish np <m.dhanish.np@gmail.com>
2026-03-27 10:00:29 +0530

    block is an expression

---
+++ block_is_an_expression/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let y = {
+        let x = 3;
+        x + 1 // if i use a ; here, It will return a () unit instead of the expression
+
+    };
+    println!("The value of y is:{y}")
+}
